e22a4bd58b79d0c55ef44610cd10052c142e3254
Shouvik Bajpayee <bajpayeeshouvik@gmail.com>
2021-04-17 13:48:47 +0530

    stable release

--- setup.py
+++ setup.py
@@ -7,25 +7,25 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.13'
-DESCRIPTION = 'Streaming video data via networks'
-LONG_DESCRIPTION = 'A package that allows to build simple streams of video, audio and camera data.'
+VERSION = '1.0.0'
+DESCRIPTION = 'Generate Hashed Stringd Easily.'
+LONG_DESCRIPTION = 'A package that allows to generate Hashed Strings in the easiest way.'
 
 # Setting up
 setup(
-    name="vidstream",
+    name="gp-hashing",
     version=VERSION,
-    author="NeuralNine (Florian Dedov)",
-    author_email="<mail@neuralnine.com>",
+    author="Shouvik Bajpayee & Sayak Mukhopadhyay",
+    author_email="<bajpayeeshouvik@gmail.com>",
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
-    install_requires=['opencv-python', 'pyautogui', 'pyaudio'],
-    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    install_requires=[],
+    keywords=['python', 'hashing', 'gp-hashing', 'generate hash', 'easy hashing'],
     classifiers=[
-        "Development Status :: 1 - Planning",
-        "Intended Audience :: Developers",
+        "Development Status :: First Stable Release",
+        "Intended Audience :: Python Programmers",
         "Programming Language :: Python :: 3",
         "Operating System :: Unix",
         "Operating System :: MacOS :: MacOS X",
